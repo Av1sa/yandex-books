a86d4fc9066ea3cd2a61340112bda69ac55479e9
Avisa <manelis1102@gmail.com>
2021-04-28 17:28:30 +0300

    feat: add app structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import Header from '../Header/Header';
+import BookList from '../BookList/BookList';
+import Preloader from '../Preloader/Preloader';
+import Modal from '../Modal/Modal';
+import { NOTHING_FOUND, SEARCH_ERROR } from '../../utils/const';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Header />
+      {found && <BookList />}
+      {isLoading && <Preloader />}
+      {nothingFound === 'no-results' && (
+        <p className="app__text">{NOTHING_FOUND}</p>
+      )}
+      {nothingFound === 'error' && <p className="app__text">{SEARCH_ERROR}</p>}
+      <Modal />
+    </div>
+  );
 }
 
 export default App;
--- src/utils/const.js
+++ src/utils/const.js
@@ -1,3 +1,6 @@
 const BASE_URL_BOOK_API = 'http://openlibrary.org/search.json';
 
-export { BASE_URL_BOOK_API };
+const NOTHING_FOUND = 'Nothing found. Try again';
+const SEARCH_ERROR = 'Something is wrong. Try again later';
+
+export { BASE_URL_BOOK_API, NOTHING_FOUND, SEARCH_ERROR };
